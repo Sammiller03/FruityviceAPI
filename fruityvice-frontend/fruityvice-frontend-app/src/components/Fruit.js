8e028dee0904bd9f8af37feb348b00b65485b364
Sammiller03 <samuelmiller442@gmail.com>
2025-06-23 22:07:40 +0100

    installed material-ui and added search a fruit component

---
+++ fruityvice-frontend/fruityvice-frontend-app/src/components/Fruit.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+export default function Fruit() {
+  return (
+    <Box
+      component="form"
+      sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Search a fruit" variant="outlined" />
+      
+    </Box>
+  );
+}
