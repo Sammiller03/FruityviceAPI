3fc3ca9d72fc8ad239c773eb1d71709f3e1b14ee
Sammiller03 <samuelmiller442@gmail.com>
2025-06-23 22:46:00 +0100

    changed width of search bar

--- fruityvice-frontend/fruityvice-frontend-app/src/components/Fruit.js
+++ fruityvice-frontend/fruityvice-frontend-app/src/components/Fruit.js
@@ -3,15 +3,15 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 
 export default function Fruit() {
+
   return (
     <Box
       component="form"
-      sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
+      sx={{ '& > :not(style)': { mt: 4, width: '50ch' } }}
       noValidate
       autoComplete="off"
     >
       <TextField id="outlined-basic" label="Search a fruit" variant="outlined" />
-      
     </Box>
   );
 }
