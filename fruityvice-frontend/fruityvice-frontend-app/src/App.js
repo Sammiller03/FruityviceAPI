b040327f03f53b88818a87b15a54f313cf81283c
Sammiller03 <samuelmiller442@gmail.com>
2025-06-25 19:16:38 +0100

    separated concerns. Moved content App.js to individual component

--- fruityvice-frontend/fruityvice-frontend-app/src/App.js
+++ fruityvice-frontend/fruityvice-frontend-app/src/App.js
@@ -1,52 +1,15 @@
-import React, { useEffect, useState } from "react";
 import "./App.css";
-import axios from "axios";
-//import fruitService from "../services/fruit.service"; //try with this later
+import FruitsList from "./components/fruits-list.component";
+import { Routes, Route } from "react-router-dom";
 
-const App = () => {
+function App() {
+  return (
+    <Routes>
+      <Route path="/all" element={<FruitsList />} />
+      
+    </Routes>
+  );
+}
 
-    //Logic ------------------------------------------------------------------------
-
-    const [items, setItems] = useState([]); //no items initially
-    const [dataIsLoaded, setDataIsLoaded] = useState(false); // false initially data has not loaded from api
-
-    // runs once when the component first renders from return html to the dom in index.html only edits the components in there
-    useEffect(() => {
-        axios
-            .get("http://localhost:8080/api/v1/fruits/all")
-            .then((res) => { //response
-                setItems(res.data); //the actual data in items now
-                setDataIsLoaded(true);
-            });
-    }, []); // the [] means only run once
-
-    if (!dataIsLoaded) {
-        return (
-            <div>
-                <h1>Finding fruits...</h1>
-            </div>
-        );
-    }
-
-    // UI --------------------------------------------------------------------
-
-    return (
-      <div className="parent">
-        <h1 className="fruits">All Fruits</h1>
-        <div className="container">
-          {items.map((item) => (
-            <div className="item" key={item._id}>
-                <p>{item.name}</p>
-                <div>
-                  <img src={item.imageURL} alt={item.name} style={{ width: 50, height: 50, padding: 10 }} />
-                </div>
-            </div>
-            
-          ))}
-          
-        </div>
-      </div>
-    );
-};
 
 export default App;
---
+++ fruityvice-frontend/fruityvice-frontend-app/src/components/fruit-detail.component.js
@@ -0,0 +1,9 @@
+
+
+function FruitDetail() {
+
+    return <h2>Fruit Detail Component !! </h2>
+}
+
+
+export default FruitDetail;
