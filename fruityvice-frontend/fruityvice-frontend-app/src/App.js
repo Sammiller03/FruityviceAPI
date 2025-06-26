e94069a241ba18df9cb01ed5042babe753363b09
Sammiller03 <samuelmiller442@gmail.com>
2025-06-26 19:15:29 +0100

    Implemented Fruit Detail component which shows the information about one fruit

--- fruityvice-frontend/fruityvice-frontend-app/src/App.js
+++ fruityvice-frontend/fruityvice-frontend-app/src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
+import FruitDetail from "./components/fruit-detail.component";
 import FruitsList from "./components/fruits-list.component";
 import { Routes, Route } from "react-router-dom";
 
+
+// :name is dynamic (e.g. "name/apple") to passed as a param to fruit-detail.component...
 function App() {
   return (
     <Routes>
       <Route path="/all" element={<FruitsList />} />
-      
+      <Route path="/test" element={<FruitDetail />} /> 
     </Routes>
   );
 }
--- fruityvice-frontend/fruityvice-frontend-app/src/components/fruit-detail.component.js
+++ fruityvice-frontend/fruityvice-frontend-app/src/components/fruit-detail.component.js
@@ -1,8 +1,37 @@
-
+import React, { useEffect, useState } from "react";
+import "../App.css"; // ../ means go back one directory
+import axios from "axios";
 
 function FruitDetail() {
 
-    return <h2>Fruit Detail Component !! </h2>
+    const [item, setItem] = useState({}) // initialize to empty object {}
+
+    useEffect(() => {
+        // inside {} is the effect.
+        
+        axios
+            .get("http://localhost:8080/api/v1/fruits/name/Tomato")
+            .then((res) => {
+                setItem(res.data); // putting the data in res (response) into the items variable to print
+            });
+        // [] is the dependancy array how many times to run [] = once.
+    }, []);
+
+    return (
+        <div className="fruit-detail-container">
+            <div className="fruit-detail-item">
+                <p>{item.name}</p>
+                <p>{item.family}</p>
+                <p>{item.order}</p>
+                <p>{item.genus}</p>
+                <p>{item.nutritions.calories}</p>
+                <p>{item.nutritions.fat}</p>
+                <p>{item.nutritions.sugar}</p>
+                <p>{item.nutritions.carbohydrates}</p>
+                <p>{item.nutritions.protein}</p>
+            </div>
+        </div>
+    );
 }
 
 
